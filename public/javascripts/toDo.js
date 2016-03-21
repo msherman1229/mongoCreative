f75a49fcc47a77991ddcc4fc7417e6c9989ead37
msherman1229 <msherman1229@gmail.com>
2016-03-21 00:29:34 +0000

    first commit

--- public/javascripts/toDo.js
+++ public/javascripts/toDo.js
@@ -21,12 +21,29 @@ $(document).ready(function(){
 		type:"GET",  
 		contentType:"application/json; charset=utf-8", 
 		success:function(data, textStatus) {
-			var everything = "<ul>";
+			var everything = "<table class='table'><th>Current To Do List</th><tbody>";
 			for (var item in data) {
-				it = data[item]; 
-				everything += "<li>ID : " + it.ID + " -- Item: " + it.Item + " --Importance " + it.Importance + " -- Completed? " + it.Completed; 
+				it = data[item];
+				var color = "white";
+				if (it.Importance == 1)
+				{
+					color = "lightblue";
+				}
+				else if (it.Importance == 2)
+				{
+					color = "purple"; 
+				}
+				else if (it.Importance == 3)
+				{
+					color = "orange";
+				}
+				else if (it.Importance == 4)
+				{
+					color = "red";
+				}
+				everything += "<tr bgcolor=" + color + "><td>ID : " + it.ID + " -- Item: " + it.Item + " --Importance " + it.Importance + " -- Completed? " + it.Completed; 
 			}
-			everything += "</ul>"; 
+			everything += "</tbody></table>"; 
 			$("#comments").html(everything); 
 		}
 	}); 
